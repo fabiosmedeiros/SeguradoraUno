bf1df00ef5867c4803e973bec04d3ecdeb8d049b
Fabio S Medeiros <fabiosmedeiros@gmail.com>
2025-12-11 10:00:21 -0300

    Criação do solution e da estrutura de pastas do projeto. Criação de projeto Web para a camada de apresentação, e projetos Class Library para as camadas de domínio e de dados.

---
+++ SeguradoraUno.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeguradoraUno.Web.MvcApplication" Language="C#" %>
---
+++ SeguradoraUno.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeguradoraUno.Web.Startup))]
+namespace SeguradoraUno.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
